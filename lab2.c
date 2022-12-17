96da4b5505d930c16288b7d0eb0db95aad80e949
qlae <77710901+qlae@users.noreply.github.com>
2022-12-16 19:29:39 -0500

    Update lab2.c

--- lab2.c
+++ lab2.c
@@ -8,7 +8,26 @@ int** read_board_from_file(char* filename){
     FILE *fp = NULL;
     int** board = NULL;
 
-    // replace this comment with your code
+    int** read_board_from_file(char* filename){
+    FILE *fp = NULL;
+    int** board = NULL;
+
+    board = (int **) malloc( sizeof(int *) * ROW_SIZE);
+    for (int i = 0; i < ROW_SIZE; i++) {
+        board[i] = (int *) malloc( sizeof(int ) * COL_SIZE);
+    }
+    if ((fp = fopen(filename,"r")) == NULL) {
+        printf("Error opening file!");
+  		exit (1);
+ 
+    }
+    for(int i =1; i < ROW_SIZE; ++i)
+    {
+       for (int j = 0; j < COL_SIZE; j++) {
+          fscanf(fp, "%d,", &board[i][j]);
+       }
+    }
+ 
 
     return board;
 }
